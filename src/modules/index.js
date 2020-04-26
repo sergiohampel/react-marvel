61ff4e5d062a42e913c81861e30e1cc63e2dcf6e
sergiohampel <sergiohampel@gmail.com>
2020-04-26 12:40:46 -0300

    feat(home): remove unused module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,4 @@
-import Home from "./Home/";
 import Character from "./Character/";
 import CharacterDetails from "./Character/details";
 
-export default [Home, Character, CharacterDetails];
+export default [Character, CharacterDetails];
