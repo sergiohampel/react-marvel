d1d48bfa0633c353ae5d9b1206f3463e7772baab
sergiohampel <sergiohampel@gmail.com>
2020-04-30 20:19:41 -0300

    Updates

---
+++ precache-manifest.ea88e2d29d76b1850660b41fdcbb1006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caa60c7ec956239def83a328d73b9ce",
+    "url": "/react-marvel/index.html"
+  },
+  {
+    "revision": "4848902726c737104975",
+    "url": "/react-marvel/static/js/2.e54e6658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-marvel/static/js/2.e54e6658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4686c65ab30cde83456a",
+    "url": "/react-marvel/static/js/main.80c4cd2c.chunk.js"
+  },
+  {
+    "revision": "6a30c9bfe004d87d27ed",
+    "url": "/react-marvel/static/js/runtime-main.ce858848.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/react-marvel/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/react-marvel/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  }
+]);
