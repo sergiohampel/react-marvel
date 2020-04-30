b132f8ce24fa7cf5447beed1c5acfaff7e9b1d0e
sergiohampel <sergiohampel@gmail.com>
2020-04-30 20:31:22 -0300

    Updates

---
+++ precache-manifest.f76873245ae7429171c27fbe017a3a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f036ecb6630cf7759a900c9c66e731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edbf7f9935ed3fe7db5",
+    "url": "/static/js/2.02e5060a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02e5060a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6cbfd61305d67cd5a8",
+    "url": "/static/js/main.cdde2690.chunk.js"
+  },
+  {
+    "revision": "eeaf9e17c28868ef20ae",
+    "url": "/static/js/runtime-main.2c9eb35c.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-marvel/precache-manifest.685197b267c2519b7c554a2bb206797c.js"
+  "/precache-manifest.f76873245ae7429171c27fbe017a3a8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-marvel/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
